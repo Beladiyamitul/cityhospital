1783c79e712572b851ee885141ac3a8a18adcdf4
dhruvgoyani7 <102339714+dhruvgoyani7@users.noreply.github.com>
2022-05-18 10:09:28 +0530

    Router-Dom

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
+import { Route, Router, Switch } from "react-router-dom";
 import Footer from "./Components/Footer/Footer";
 import Header from "./Components/Header/Header";
+import Department from "./Container/Department/Department";
+import Doctor from "./Container/Doctor/Doctor";
+import About from "./Container/About/About";
+import Contact from "./Container/Contact/Contact";
 import Home from "./Container/Home/Home";
+
 
 
 function App() {
   return (
    <>
-    <Header/>
-    <Home/>
+     <Header/>
+        <Switch>
+          <Route exact path={"/"} component={Home}/>
+          <Route exact path={"/department"} component={Department}/>
+          <Route exact path={"/doctor"} component={Doctor}/>
+          <Route exact path={"/about"} component={About}/>
+          <Route exact path={"/contact"} component={Contact}/>
+
+  
+        </Switch>
     <Footer/>
    
    

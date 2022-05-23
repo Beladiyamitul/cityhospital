d6dc2295af3552ea8688a87c6c8710853ef82fbf
dhruvgoyani7 <102339714+dhruvgoyani7@users.noreply.github.com>
2022-05-23 10:58:09 +0530

    login-signup

--- src/Container/Auth/Auth.js
+++ src/Container/Auth/Auth.js
@@ -1,41 +1,61 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button , Form , FormGroup , Input , Label} from 'reactstrap';
 
 function Auth(props) {
+
+  const [User, setUserType] = useState('login')
+  const [Reset , setReset] = useState(false)
+
     return (
         
-        <div className='container p-5'>
-            <h3 className='text-center'>Log In</h3>
-             <Form inline>
-    <FormGroup floating>
-      <Input
-        id="exampleEmail"
-        name="email"
-        placeholder="Email"
-        type="email"
-      />
-      <Label for="exampleEmail">
-        Email
-      </Label>
-    </FormGroup>
-    {' '}
-    <FormGroup floating>
-      <Input
-        id="examplePassword"
-        name="password"
-        placeholder="Password"
-        type="password"
-      />
-      <Label for="examplePassword">
-        Password
-      </Label>
-    </FormGroup>
-    {' '}
-    <Button>
-      Submit
-    </Button>
-  </Form>
-  </div>
+      <section id="appointment" className="appointment">
+          <div className="container">
+              {
+                
+                User === 'login' ?
+                  <div className="section-title">
+                      <h2>Login</h2>
+                  </div>
+                  :  <div className="section-title">
+                       <h2>Signup</h2>
+                      </div>
+              }
+             
+            <form action method="post" role="form" className="php-email-form">
+              {
+                User === "Signup" ?
+                <div className="row justify-content-center"> 
+                    <div className="col-md-4 form-group">
+                      <input type="text" name="name" className="form-control" id="name" placeholder="Your Name" data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
+                      <div className="validate" />
+                    </div> 
+                   </div> 
+                :null
+              }
+            
+              <div className="row justify-content-center">  
+                <div className="col-md-4 form-group mt-3 mt-md-0">
+                  <input type="email" className="form-control" name="email" id="email" placeholder="Your Email" data-rule="email" data-msg="Please enter a valid email" />
+                  <div className="validate" />
+                </div>
+              </div>
+              <div className="row justify-content-center"> 
+                <div className="col-md-4 form-group mt-3 mt-md-0">
+                  <input type="tel" className="form-control" name="phone" id="phone" placeholder="Your Phone" data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
+                  <div className="validate" />
+                </div>
+              </div>
+              <div className='row'>
+                <div className='d-flex  justify-content-center'>
+                <div class="text-center login m-3"><button type="button" onClick={()=> setUserType('login')}>Login</button></div>
+                <div class="text-center signup m-3"><button type="button" onClick={()=> setUserType('Signup')}>Signup</button></div>
+                </div>
+
+              </div>
+            </form>
+          </div>
+       </section>
+
        
     );
 }

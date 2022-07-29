5ef32e9aed0a20cc0af2a64356798d8277f02449
Beladiyamitul <mitulbeladiya812@gmail.com>
2022-07-29 08:14:46 +0530

    fix

--- src/Redux/ReduxSaga/auth.saga.js
+++ src/Redux/ReduxSaga/auth.saga.js
@@ -1,12 +1,13 @@
 import { call, put, takeEvery ,all } from 'redux-saga/effects'
 import { LoginAPI } from '../../common/api/auth.api';
+import { emailVerify } from '../Action/Auth.action';
 import * as ActionType from "../ActionType"
 
 
 function* loginsaga(action) {
    try {
-      const user = yield call(LoginAPI(), action.payload);
-      yield put({type: ActionType.EMAIL_VERIFY, payload: user});
+      const user = yield call(LoginAPI, action.payload);
+      yield put(emailVerify(user));
    } catch (e) {
       yield put({type: "USER_FETCH_FAILED", message: e.message});
    }
